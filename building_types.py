9df786127023d9813aacf78f3ee54764c7ca8721
ProximaPrism <s10267229@connect.np.edu.sg>
2026-06-30 13:56:11 +0800

    feat: update classes, progress in FP game structure

--- building_types.py
+++ building_types.py
@@ -3,27 +3,29 @@ from abc import ABC, abstractmethod
 
 # abstract class that buildings used, should NOT be initialized on its own
 class Building(ABC):
+    name: str
     symbol: str
 
     @abstractmethod
     def score(self, grid, x: int, y: int):
         pass
 
-    def coins(self, grid, x: int, y: int):
+    def income(self, grid, x: int, y: int):
         return 0
 
 
 class Residential(Building):
+    name = "Residential"
     symbol = "R"
 
     def score(self, grid, x, y):
         # if Residential is adjacent to Industry, force score to 1 point
-        if any(isinstance(b, Industry) for b in grid.direct_adjacent(x, y)):
+        if any(isinstance(b, Industry) for _, _, b in grid.direct_adjacent(x, y)):
             return 1
 
         score = 0
         # Buildings adjacent via Road, 1 point per Residential / Commerical and 2 points per Park
-        for b in grid.road_adjacent(x, y):
+        for _, _, b in grid.road_adjacent(x, y):
             if isinstance(b, (Residential, Commercial)):
                 score += 1
             elif isinstance(b, Park):
@@ -33,6 +35,7 @@ class Residential(Building):
 
 
 class Industry(Building):
+    name = "Industry"
     symbol = "I"
 
     # An Industry scores 1 point on its own. No adjacency bonuses.
@@ -40,31 +43,34 @@ class Industry(Building):
         return 1
 
     # Each Industry generates 1 coin per Residential building adjacent
-    def coins(self, grid, x, y):
-        return sum(isinstance(b, Residential) for b in grid.road_adjacent(x, y))
+    def income(self, grid, x, y):
+        return sum(isinstance(b, Residential) for _, _, b in grid.road_adjacent(x, y))
 
 
 class Commercial(Building):
+    name = "Commercial"
     symbol = "C"
 
     # Commerical scores 1 point per adjacent Commercial building
     def score(self, grid, x, y):
-        return sum(isinstance(b, Commercial) for b in grid.road_adjacent(x, y))
+        return sum(isinstance(b, Commercial) for _, _, b in grid.road_adjacent(x, y))
 
     # Each Commerical generates 1 coin per Residential building adjacent
-    def coins(self, grid, x, y):
-        return sum(isinstance(b, Residential) for b in grid.road_adjacent(x, y))
+    def income(self, grid, x, y):
+        return sum(isinstance(b, Residential) for _, _, b in grid.road_adjacent(x, y))
 
 
 class Park(Building):
+    name = "Park"
     symbol = "O"
 
     # Park scores 1 point per adjacent Park building
     def score(self, grid, x, y):
-        return sum(isinstance(b, Park) for b in grid.road_adjacent(x, y))
+        return sum(isinstance(b, Park) for _, _, b in grid.road_adjacent(x, y))
 
 
 class Road(Building):
+    name = "Road"
     symbol = "*"
 
     # Roads score 1 point per connected road within the same row.
